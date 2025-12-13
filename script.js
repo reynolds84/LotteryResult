e1a0f6ef6c486d190962ed97ef1939d7f74c0f17
reynolds84 <80551326+reynolds84@users.noreply.github.com>
2025-12-12 21:56:06 -0500

    Refactor fetch logic and HTML table creation

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const SUPABASE_URL = "https://cjwkmtqpzvkpxjlfcyzg.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqd2ttdHFwenZrcHhqbGZjeXpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwNDEwMDMsImV4cCI6MjA3NTYxNzAwM30.vFwABypAsnw15vMpo3_A--ayRwUkq6iQJYUXBbbQxs8";
 
 // --- OBTENER DATOS DE SUPABASE ---
-fetch(`${SUPABASE_URL}/rest/v1/Result?select=fecha,dia_noche,pick3,pick4,lottery&order=fecha.desc, dia_noche.desc&limit=60`, {
-    headers: {
+fetch
+(
+    `${SUPABASE_URL}/rest/v1/Result?select=fecha,dia_noche,pick3,pick4,lottery&order=fecha.desc, dia_noche.desc&limit=60`,
+    {
+    headers: 
+    {
         apikey: SUPABASE_KEY,
         Authorization: `Bearer ${SUPABASE_KEY}`
     }
@@ -47,6 +51,7 @@ fetch(`${SUPABASE_URL}/rest/v1/Result?select=fecha,dia_noche,pick3,pick4,lottery
     console.error(err);
     document.getElementById("Result").innerHTML = "<p>Error cargando resultados</p>";
 });
+
 
 
 
